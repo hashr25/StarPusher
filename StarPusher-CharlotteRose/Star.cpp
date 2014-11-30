a3e5a827ecd1d3e23d55a782029a994b5334c843
Charlotte Rose Hamilton <charlottesemail@icloud.com>
2014-11-30 18:11:51 -0500

    Revert "to merge"
    This reverts commit 29114fe18cdc43db04950c9220b6a7a0c22eb58c.

---
+++ StarPusher-CharlotteRose/Star.cpp
@@ -0,0 +1,63 @@
+#include "Star.h"
+
+/// //////////////////////////////////////////////////////////////////////////////
+///Constructors
+Star::Star()
+{
+    isAtGoal = false;
+}
+
+Star::Star( int x, int y )
+{
+    this -> x = x;
+    this -> y = y;
+}
+
+/// /////////////////////////////////////////////////////////////////////////////
+///Getters and Setters
+//Getters
+bool Star::getIsAtGoal() const
+{
+    return isAtGoal;
+}
+
+//Setters
+void Star::setIsAtGoal(bool isAtGoal)
+{
+    this -> isAtGoal = isAtGoal;
+}
+
+bool Star::isBlocked( Direction directionToCheckS )
+{
+    bool isBlocked = false;
+
+
+    return isBlocked;
+}
+
+void Star::push( Direction directionToPush )
+{
+    switch ( directionToPush )
+    {
+    case NORTH:
+        {
+            //setMapY( getMapY() - 1 );
+            break;
+        }
+    case EAST:
+        {
+            //setMapX( getMapX() + 1 );
+            break;
+        }
+    case WEST:
+        {
+            //setMapX( getMapX() - 1 );
+            break;
+        }
+    case SOUTH:
+        {
+            //setMapY( getMapY() + 1 );
+            break;
+        }
+    }
+}
---
+++ StarPusher-CharlotteRose/main.cpp
@@ -0,0 +1,26 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2014)
+and may not be redistributed without written permission.*/
+
+//Using SDL, SDL_image, standard IO, strings, and file streams
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "LTexture.h"
+#include "Tile.h"
+#include "EnumTypes.h"
+#include "Player.h"
+#include "GameController.h"
+
+void rungame();
+
+int main( int argc, char* args[] )
+{
+	GameController StarPusher;
+	StarPusher.runGame();
+    //    std::cout << "How many levels: " << StarPusher.getLevels().size() << std:: endl;
+	return 0;
+}
+
